07ca4602513be45dce113ee006a01962d68c2c32
Ashish-bhandarii <140409097+Ashish-bhandarii@users.noreply.github.com>
2025-03-28 17:41:39 +0545

    Update submit.php

--- submit.php
+++ submit.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->Host       = 'smtp.gmail.com';    
             $mail->SMTPAuth   = true;
             $mail->Username   = '124ashishbhandari@gmail.com';  
-            $mail->Password   = 'ahto nafr wdgm fcbs';            
+            $mail->Password   = '';            
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port       = 587;
 
